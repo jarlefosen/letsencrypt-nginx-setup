bf719608c33a7bffd38f478b5403f7bddf99df60
Jarle Fosen <jarlefosen@gmail.com>
2016-01-29 19:51:45 +0100

    Added TOS agreement and email to letsencrypt command

--- request-certificate/request.sh
+++ request-certificate/request.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-DOMAIN_FILE=$1
+# Your email address, used for recovery and revoking certs
+LE_ACCOUNT="name@example.com"
 WELL_KNOWN_DIR="/opt/ssl-challenge/"
-LETSENCRYPT_ACCOUNT="name@example.com"
+LE_EXECUTABLE="letsencrypt-auto" # Path to your script if not in env PATH
+
+# No options specifies production certificates
+SERVER_OPTIONS=''
+# Specifies staging certificates, not usable in production
+# Comment out the line below for production certificates.
 SERVER_OPTIONS='--server https://acme-staging.api.letsencrypt.org/directory'
+
+
+print_usge() {
+    echo "Usage: ./request.sh <domain_file>"
+    echo "domain_file contains a list of domains you want included in your"
+    echo "SSL certificate"
+    echo "The first item in the list will be treated as a CN"
+}
 
 read_domains() {
 
@@ -19,13 +33,15 @@ read_domains() {
     echo $DOMAINS
 }
 
+# Check that the given domain file exists
+DOMAIN_FILE=$1
 if [ ! -f $DOMAIN_FILE ]; then
     echo 'Specified file "'$DOMAIN_FILE'" does not exist!'
     return 1
 fi
 
 DOMAIN_NAME=$(read_domains $DOMAIN_FILE)
-UPDATE_COMMAND="letsencrypt-auto certonly --webroot --renew-by-default $SERVER_OPTIONS -w $WELL_KNOWN_DIR $DOMAIN_NAME"
+UPDATE_COMMAND="letsencrypt-auto certonly --webroot --renew-by-default --agree-tos --email $LE_ACCOUNT $SERVER_OPTIONS -w $WELL_KNOWN_DIR $DOMAIN_NAME"
 RELOAD_WEBSERVER='service nginx reload'
 
 $UPDATE_COMMAND
